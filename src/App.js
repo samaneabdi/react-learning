0b2f0707610fde0c2c48a14c38f21e394580583e
samane.saremi1992 <samane.saremi1992@gmail.com>
2023-08-21 16:13:29 +0330

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Child from "./Child";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
+const fibn = (n) => {
+  if (n == 1 || n == 0) return 1;
+  return fibn(n - 1) + fibn(n - 2);
+};
 function App() {
+  console.log("parent is rendering");
+  const [counter, setCounter] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [value, setValue] = useState(1);
+  const [show, setShow] = useState(true);
+  const addCounter = useCallback(() => {
+    setCounter(counter + 1);
+  }, [counter]);
+  const fib = useMemo(() => {
+    return fibn(Number(value));
+  }, [value]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+    console.log("parent is mount");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {show && <Child addCounter={addCounter} />}
+        {show && <Child addCounter={addCounter} />}
+
+        <button type="button" onClick={() => setCounter(counter + 1)}>
+          click
+        </button>
+        <button onClick={() => setShow(!show)}>clickShow</button>
+        <div>{date.toISOString()}</div>
+        <div>{counter}</div>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <div>{fib}</div>
       </header>
     </div>
   );
---
+++ src/Child.jsx
@@ -0,0 +1,35 @@
+import userEvent from "@testing-library/user-event";
+import { useEffect, useRef } from "react";
+import { memo } from "react";
+let global = 0;
+const Child = ({addCounter}) =>{
+    
+    console.log("child is rendering");
+    const ref = useRef(0);
+    useEffect(() => {
+        
+        console.log("child is mount");
+        return() =>{
+
+            console.log("child is unmount");
+        }
+      }, []);
+
+    return (
+        <>
+        <button onClick={()=>addCounter()}>counter</button>
+        <div>child</div>
+        <button onClick={()=>{
+            ref.current += 1;
+            console.log(ref.current)
+        }}>addRef</button>
+        <div>{ref.current}</div>
+        <button onClick={()=>{
+            global += 1;
+            console.log(global)
+        }}>addGlobal</button>
+        <div>{global}</div>
+        </>
+    )
+}
+export default memo(Child);
