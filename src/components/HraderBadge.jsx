11610a141ee0a9b60402db9ab449a45e119b2699
samane.saremi1992 <samane.saremi1992@gmail.com>
2023-09-14 12:44:38 +0330

    fixmobxbug

--- src/components/HraderBadge.jsx
+++ src/components/HraderBadge.jsx
@@ -1,5 +1,6 @@
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { Badge } from "antd";
+import { useObserver } from "mobx-react";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { productStore } from "../store/productStore";
@@ -8,11 +9,10 @@ import { productStore } from "../store/productStore";
 const HeaderBadge = () =>{
     const navigate = useNavigate();
     const store = useContext(productStore);
-    const totalSelectedProducts = store.selectedProducts.reduce((total, item) => total + item.quantity, 0);
-    return (
-        <Badge size="small" count={totalSelectedProducts}>
+    return useObserver(() => (
+      <Badge size="small" count={store.totalSelectedProducts}>
         <ShoppingCartOutlined onClick={() => navigate("/ProductBasket")} className="btnIcon"/>
       </Badge>
-    )
+    ))
 }
 export default HeaderBadge;
--- src/store/productStore.js
+++ src/store/productStore.js
@@ -9,13 +9,16 @@ const store = useLocalStore(() =>(
         products: productInitialList,
         selectedProducts: [],
         get totalPrice(){
-            return 0;
+            return store.selectedProducts.reduce((totalPrice, item) => totalPrice += (item.quantity * item.price), 0);
+        },
+        get totalSelectedProducts(){
+            return store.selectedProducts.reduce((total, item) => total + item.quantity, 0);
         },
         addToProductBasket: ({product}) => {
-            const selectedProductIndex = store.selectedProducts.findIndex(x=>x.id == product.id);
-            if(selectedProductIndex > -1)
+            const existingProductIndex = store.selectedProducts.findIndex(x=> x.id === product.id);
+            if(existingProductIndex > -1)
             {
-                const product = store.selectedProducts[selectedProductIndex];
+                const product = store.selectedProducts[existingProductIndex];
                 product.quantity += 1;
                 store.selectedProducts=[...store.selectedProducts];
             }
@@ -25,8 +28,31 @@ const store = useLocalStore(() =>(
                 store.selectedProducts = [...store.selectedProducts, product]
             }
         },
-        removeFromProductBasket: (product) =>{
-            console.log({product});
+        removeFromProductBasket: ({product}) =>{
+            const existingProductIndex = store.selectedProducts.findIndex(x=> x.id === product.id);
+
+            if (existingProductIndex !== -1) {
+                if (store.selectedProducts[existingProductIndex].quantity > 1) {
+                    console.log("aa",store.selectedProducts);
+                    const selectedProduct = store.selectedProducts[existingProductIndex];
+                    selectedProduct.quantity -= 1;
+                    store.selectedProducts=[...store.selectedProducts];
+                } else {
+                    console.log("bb",store.selectedProducts);
+                    const selectedProduct = store.selectedProducts.splice(existingProductIndex, 1);
+                    store.selectedProducts=[...selectedProduct];
+
+                }
+            }
+
+            const productIndex = store.products.findIndex((p) => p.id === product.id);
+            if (productIndex !== -1 && store.products[productIndex].quantity > 0) {
+                const product = store.products[productIndex];
+                console.log("c",product.quantity);
+                product.quantity -= 1;
+                console.log("d",product.quantity);
+                store.products=[...store.products];
+            }
         }
     }
 ));
