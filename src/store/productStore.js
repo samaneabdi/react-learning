1de37777aa19ab5acaec380f0eed8a00c62e27cc
samane.saremi1992 <samane.saremi1992@gmail.com>
2023-09-15 11:24:29 +0330

    fix-bug

--- src/store/productStore.js
+++ src/store/productStore.js
@@ -16,14 +16,13 @@ const store = useLocalStore(() =>(
         },
         addToProductBasket: ({product}) => {
             const existingProductIndex = store.selectedProducts.findIndex(x=> x.id === product.id);
-            if(existingProductIndex > -1)
+            if(existingProductIndex !== -1)
             {
-                const product = store.selectedProducts[existingProductIndex];
-                product.quantity += 1;
+                const selectedProduct = store.selectedProducts[existingProductIndex];
+                selectedProduct.quantity += 1;
                 store.selectedProducts=[...store.selectedProducts];
             }
             else{
-                console.log({product});
                 product.quantity = 1;
                 store.selectedProducts = [...store.selectedProducts, product]
             }
@@ -33,25 +32,16 @@ const store = useLocalStore(() =>(
 
             if (existingProductIndex !== -1) {
                 if (store.selectedProducts[existingProductIndex].quantity > 1) {
-                    console.log("aa",store.selectedProducts);
                     const selectedProduct = store.selectedProducts[existingProductIndex];
                     selectedProduct.quantity -= 1;
                     store.selectedProducts=[...store.selectedProducts];
                 } else {
-                    console.log("bb",store.selectedProducts);
-                    const selectedProduct = store.selectedProducts.splice(existingProductIndex, 1);
-                    store.selectedProducts=[...selectedProduct];
+                    const selectedProduct = store.selectedProducts[existingProductIndex];
+                    selectedProduct.quantity -= 1;
+                    store.selectedProducts.splice(existingProductIndex, 1);
+                    store.selectedProducts=[...store.selectedProducts];
 
                 }
-            }
-
-            const productIndex = store.products.findIndex((p) => p.id === product.id);
-            if (productIndex !== -1 && store.products[productIndex].quantity > 0) {
-                const product = store.products[productIndex];
-                console.log("c",product.quantity);
-                product.quantity -= 1;
-                console.log("d",product.quantity);
-                store.products=[...store.products];
             }
         }
     }
