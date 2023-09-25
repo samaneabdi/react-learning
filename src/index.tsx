0fd7761d54403d51696b65dc936e9f5da146f0c3
samane.saremi1992 <samane.saremi1992@gmail.com>
2023-09-25 16:04:31 +0330

    react-router-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/route";
 import Loading from "./components/Loading";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Suspense fallback={<Loading />}>
-    <RouterProvider router={router} />
-  </Suspense>
-);
+const rootDiv = document.getElementById("root");
+if(rootDiv){
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
