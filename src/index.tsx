6828904d178d85fa27718281536c46c13a482645
samane.saremi1992 <samane.saremi1992@gmail.com>
2023-09-27 21:35:10 +0330

    convertjstots

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/route";
 import Loading from "./components/Loading";
@@ -11,7 +10,7 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Suspense fallback={<Loading />}>
     <Provider store={store}>
@@ -26,4 +25,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
